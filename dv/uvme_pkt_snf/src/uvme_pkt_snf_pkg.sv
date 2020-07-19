e78fb88e021b1ee5eb8cd492a217ba254fca7822
David Poulin <dpoulin@datumtc.ca>
2020-07-19 13:33:55 -0400

    Updating license headers to SolderPad 2.1

--- dv/uvme_pkt_snf/src/uvme_pkt_snf_pkg.sv
+++ dv/uvme_pkt_snf/src/uvme_pkt_snf_pkg.sv
@@ -1,17 +1,18 @@
 // 
 // Copyright 2020 Datum Technology Corporation
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 // 
-// Licensed under the Solderpad Hardware License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may
+// not use this file except in compliance with the License, or, at your option,
+// the Apache License version 2.0. You may obtain a copy of the License at
 // 
-//     https://solderpad.org/licenses/
+//     https://solderpad.org/licenses/SHL-2.1/
 // 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, any work
+// distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
 // 
 
 
@@ -24,10 +25,7 @@
 `include "uvml_hrtbt_macros.sv"
 `include "uvme_pkt_snf_macros.sv"
 
-// Interface(s) / Module(s) / Checker(s)
-`ifdef UVME_PKT_SNF_INC_CHKR
-`include "uvma_pkt_snf_chkr.sv"
-`endif
+// Interface(s)
 
 
  /**
@@ -72,6 +70,12 @@ package uvme_pkt_snf_pkg;
    `include "uvme_pkt_snf_vseq_lib.sv"
    
 endpackage : uvme_pkt_snf_pkg
+
+
+// Module(s) / Checker(s)
+`ifdef UVME_PKT_SNF_INC_CHKR
+`include "uvma_pkt_snf_chkr.sv"
+`endif
 
 
 `endif // __UVME_PKT_SNF_PKG_SV__
