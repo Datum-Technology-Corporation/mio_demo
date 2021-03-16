81d8893b38d6f40f11a7bdb17882ab5e447da89c
David Poulin <dpoulin@datumtc.ca>
2021-03-16 12:49:22 -0400

    Updated year in copyright notice

--- dv/uvmt_pkt_snf/src/tb/uvmt_pkt_snf_tb.sv
+++ dv/uvmt_pkt_snf/src/tb/uvmt_pkt_snf_tb.sv
@@ -1,5 +1,5 @@
 // 
-// Copyright 2020 Datum Technology Corporation
+// Copyright 2021 Datum Technology Corporation
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 // 
 // Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may
