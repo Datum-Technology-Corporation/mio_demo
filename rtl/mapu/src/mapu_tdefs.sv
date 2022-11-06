0846133c794159ca790366ca11063f9a892cdee5
datum-dpoulin <dpoulin@datumtc.ca>
2022-11-06 00:53:22 -0400

    Adding flow control to mapu_top

--- rtl/mapu/src/mapu_tdefs.sv
+++ rtl/mapu/src/mapu_tdefs.sv
@@ -7,12 +7,7 @@
 `define __MAPU_TDEFS_SV__
 
 
-typedef enum logic [1:0] {
-   MAPU_STATE_LOADING   = 2'b00,
-   MAPU_STATE_LOADED    = 2'b01,
-   MAPU_STATE_UNLOADING = 2'b10,
-   MAPU_STATE_DONE      = 2'b11
-} mapu_state_enum;
+
 
 
 `endif // __MAPU_TDEFS_SV__
--- rtl/mapu/src/mapu_top.sv
+++ rtl/mapu/src/mapu_top.sv
@@ -15,30 +15,33 @@
  * Matrix APU operating on 4x4 matrices of unsigned integers.
  */
 module mapu_top # (
-   parameter DATA_WIDTH = 8
+   parameter DATA_WIDTH = 32
 ) (
-   input                      clk     , ///< Clock
-   input                      reset_n , ///< Reset
-   input                      i_en    , ///< Block enable
-   input  [1:0]               i_op    , ///< Matrix operation to be performed
-   input  [(DATA_WIDTH-1):0]  i_r0    , ///< Input row element 0
-   input  [(DATA_WIDTH-1):0]  i_r1    , ///< Input row element 1
-   input  [(DATA_WIDTH-1):0]  i_r2    , ///< Input row element 2
-   input  [(DATA_WIDTH-1):0]  i_r3    , ///< Input row element 3
-   output                     o_vld   , ///< Indicates data output lines are valid
-   output [(DATA_WIDTH-1):0]  o_r0    , ///< Output row element 0
-   output [(DATA_WIDTH-1):0]  o_r1    , ///< Output row element 1
-   output [(DATA_WIDTH-1):0]  o_r2    , ///< Output row element 2
-   output [(DATA_WIDTH-1):0]  o_r3      ///< Output row element 3
+   input                      clk    , ///< Clock
+   input                      reset_n, ///< Reset
+   input                      i_en   , ///< Block enable
+   input  [1:0]               i_op   , ///< Matrix operation to be performed
+   input                      i_vld  , ///< Indicates data input lines are valid
+   input                      i_rdy  , ///< Flow control from downstream block
+   input  [(DATA_WIDTH-1):0]  i_r0   , ///< Input row element 0
+   input  [(DATA_WIDTH-1):0]  i_r1   , ///< Input row element 1
+   input  [(DATA_WIDTH-1):0]  i_r2   , ///< Input row element 2
+   input  [(DATA_WIDTH-1):0]  i_r3   , ///< Input row element 3
+   output                     o_vld  , ///< Indicates data output lines are valid
+   output                     o_rdy  , ///< Flow control for upstream block
+   output [(DATA_WIDTH-1):0]  o_r0   , ///< Output row element 0
+   output [(DATA_WIDTH-1):0]  o_r1   , ///< Output row element 1
+   output [(DATA_WIDTH-1):0]  o_r2   , ///< Output row element 2
+   output [(DATA_WIDTH-1):0]  o_r3     ///< Output row element 3
 );
 
-   mapu_state_enum               state;
-   reg [2:0]                     row_count;
-   reg                           b_o_vld;
+   reg [2:0]                     row_count_i, row_count_o;
+   reg                           b_o_vld, b_o_rdy;
    reg [(DATA_WIDTH-1):0]        b_o_r0, b_o_r1, b_o_r2, b_o_r3;
    reg [(DATA_WIDTH-1):0][4][4]  ma, mb, mc;
 
    assign o_vld  = b_o_vld;
+   assign o_rdy  = b_o_rdy;
    assign o_r0   = b_o_r0 ;
    assign o_r1   = b_o_r1 ;
    assign o_r2   = b_o_r2 ;
@@ -46,25 +49,35 @@ module mapu_top # (
 
    always @(posedge clk) begin
       if (reset_n === 0) begin
-         state     <= MAPU_STATE_LOADING;
-         row_count <= 0;
-         b_o_vld   <= 0;
-         b_o_r0    <= 0;
-         b_o_r1    <= 0;
-         b_o_r2    <= 0;
-         b_o_r3    <= 0;
+         row_count_i <= 0;
+         row_count_o <= 4;
+         b_o_vld     <= 0;
+         b_o_rdy     <= 0;
+         b_o_r0      <= 0;
+         b_o_r1      <= 0;
+         b_o_r2      <= 0;
+         b_o_r3      <= 0;
          clear_matrices();
       end
    end
 
    always @(posedge clk) begin
       if (reset_n === 1) begin
-         case (state)
-            MAPU_STATE_LOADING  : fsm_loading  ();
-            MAPU_STATE_LOADED   : fsm_loaded   ();
-            MAPU_STATE_UNLOADING: fsm_unloading();
-            MAPU_STATE_DONE     : fsm_done     ();
-         endcase
+         load_row();
+         if (i_en === 1) begin
+            if (row_count_o === 4) begin
+               do_op();
+            end
+            else begin
+               unload_row();
+            end
+         end
+         else begin
+            b_o_vld <= 0;
+         end
+      end
+      else begin
+         b_o_vld <= 0;
       end
    end
 
@@ -86,70 +99,50 @@ module mapu_top # (
       end
    endfunction : clear_matrices
 
-   function void fsm_loading();
-      if (en === 1) begin
-         if (row_count < 3) begin
+   function void load_row();
+      if (i_vld === 1) begin
+         if (row_count_i < 3) begin
             ma[row_count][0] <= i_r0;
             ma[row_count][1] <= i_r1;
             ma[row_count][2] <= i_r2;
             ma[row_count][3] <= i_r3;
+            row_count_i <= row_count_i + 1;
+            b_o_rdy <= 1;
          end
-         else begin
+         else if (row_count_i inside {[3:6]}) begin
             mb[row_count][0] <= i_r0;
             mb[row_count][1] <= i_r1;
             mb[row_count][2] <= i_r2;
             mb[row_count][3] <= i_r3;
+            row_count_i <= row_count_i + 1;
+            b_o_rdy <= 1;
          end
-
-         row_count <= row_count + 1;
-         if (row_count == (7-1)) begin
-            state <= MAPU_STATE_LOADED;
+         else begin
+            b_o_rdy <= 0;
          end
       end
-      b_o_vld <= 0;
-   endfunction : fsm_init
+   endfunction : load_row
 
-   function void fsm_loaded();
-      if (en === 1) begin
-         case (op)
-            `MAPU_OP_ADD : do_add ();
-            `MAPU_OP_SUB : do_sub ();
-            `MAPU_OP_MULT: do_mult();
-         endcase
-         state     <= MAPU_STATE_UNLOADING;
-         row_count <= 3;
-         b_o_vld   <= 1;
-      end
-      else begin
-         b_o_vld <= 0;
-      end
-   endfunction : fsm_loaded
-
-   function void fsm_unloading();
-      if (en) begin
+   function void unload_row();
+      if (i_rdy === 1) begin
          b_o_vld <= 1;
-         b_o_r0  <= mc[row_count][0];
-         b_o_r1  <= mc[row_count][1];
-         b_o_r2  <= mc[row_count][2];
-         b_o_r3  <= mc[row_count][3];
-         row_count <= row_count - 1;
-         if (row_count == 1) begin
-            state <= MAPU_STATE_DONE;
-         end
+         b_o_r0  <= mc[row_count_o][0];
+         b_o_r1  <= mc[row_count_o][1];
+         b_o_r2  <= mc[row_count_o][2];
+         b_o_r3  <= mc[row_count_o][3];
+         row_count_o <= row_count_o + 1;
       end
-      else begin
-         b_o_vld <= 0;
-      end
-   endfunction : fsm_unloading
+   endfunction : unload_row
 
-   function void fsm_done();
-      clear_matrices();
-      state   <= MAPU_STATE_LOADING;
+   function void do_op();
       b_o_vld <= 0;
-      b_o_r0  <= 0;
-      b_o_r1  <= 0;
-      b_o_r2  <= 0;
-   endfunction : fsm_done
+      case (i_op)
+         `MAPU_OP_ADD : do_add ();
+         `MAPU_OP_SUB : do_sub ();
+         `MAPU_OP_MULT: do_mult();
+      endcase
+      row_count_o <= 0;
+   endfunction : do_op
 
    function void do_add();
       mc[0][0] <= ma[0][0] + mb[0][0];
