8d466d96bfb7bacb85f3af69fef2065ba62d7d0d
David Poulin <55469439+datum-dpoulin@users.noreply.github.com>
2022-11-22 23:38:33 -0500

    Update tb.sv

--- dv/tb/src/tb.sv
+++ dv/tb/src/tb.sv
@@ -1,11 +1,11 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
 `include "hello_world_test.sv"
 
 module tb;
-   import uvm_pkg::*;
-   
    logic  clk, reset_n;
    top    dut(.*);
-   
+
    initial begin
       clk = 0;
       forever begin
@@ -13,15 +13,15 @@ module tb;
          clk = !clk;
       end
    end
-   
+
    initial begin
       reset_n = 0;
       #100ns;
       reset_n = 1;
    end
-   
+
    initial begin
       uvm_top.run_test();
    end
-   
+
 endmodule
