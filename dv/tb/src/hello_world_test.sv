0a9e1bef682e0fe59ecd1920b777e3b649704818
David Poulin <55469439+datum-dpoulin@users.noreply.github.com>
2022-11-22 23:38:21 -0500

    Update hello_world_test.sv

--- dv/tb/src/hello_world_test.sv
+++ dv/tb/src/hello_world_test.sv
@@ -1,13 +1,11 @@
-import uvm_pkg::*;
-
 class hello_world_test_c extends uvm_test;
-   
-   `uvm_component_utils_begin(hello_world_test_c)
-   
+
+   `uvm_component_utils(hello_world_test_c)
+
    function new(string name="hello_world_test", uvm_component parent=null);
       super.new(name, parent);
    endfunction
-   
+
    virtual task run_phase(uvm_phase phase);
       phase.raise_objection(this);
       #1_000ns;
@@ -15,5 +13,5 @@ class hello_world_test_c extends uvm_test;
       #1_000ns;
       phase.drop_objection(this);
    endtask
-   
+
 endclass
