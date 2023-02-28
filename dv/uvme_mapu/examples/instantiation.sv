1be488938c351486eb77a0de5b0b751f8a53ced4
datum-dpoulin <dpoulin@datumtc.ca>
2023-02-28 15:13:51 -0500

    adding missing example file for uvme_mapu

---
+++ dv/uvme_mapu/examples/instantiation.sv
@@ -0,0 +1,129 @@
+// Copyright 2023 Acme Enterprises Inc.
+// All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This file contains sample code that demonstrates how to add an instance of the Matrix APU Block UVM Environment to an example UVMx environment.
+// NOTE: It is recommended to split up classes into separate files.
+
+
+`ifndef __UVME_EXAMPLE_ENV_CFG_SV__
+`define __UVME_EXAMPLE_ENV_CFG_SV__
+
+
+/**
+ * Object encapsulating all configuration information for uvme_example_env_c.
+ */
+class uvme_example_env_cfg_c extends uvmx_env_cfg_c;
+
+   rand uvme_mapu_cfg_c  mapu_cfg; ///< Handle to mapu environment configuration
+
+   `uvm_object_utils_begin(uvme_example_env_cfg_c)
+      `uvm_field_object(mapu_cfg, UVM_DEFAULT)
+   `uvm_object_utils_end
+
+   /**
+    * Rules for safe default options
+    */
+   constraint defaults_cons {
+      soft mapu_cfg.enabled == 1;
+   }
+
+   /**
+    * Default constructor.
+    */
+   function new(uvm_component parent=null, string name="uvme_example_env_cfg");
+      super.new(parent, name);
+   endfunction
+
+   /**
+    * Creates sub-configuration objects
+    */
+   virtual function void create_objects();
+      mapu_cfg = uvme_mapu_cfg_c::type_id::create("mapu_cfg");
+   endfunction
+
+endclass : uvme_example_env_cfg_c
+
+
+`endif // __UVME_EXAMPLE_ENV_CFG_SV__
+
+
+`ifndef __UVME_EXAMPLE_ENV_CNTXT_SV__
+`define __UVME_EXAMPLE_ENV_CNTXT_SV__
+
+
+/**
+ * Object encapsulating all state variables for uvme_example_env_c.
+ */
+class uvme_example_env_cntxt_c extends uvmx_env_cntxt_c;
+
+   uvme_mapu_cntxt_c  mapu_cntxt; ///< Handle to mapu environment context
+
+   `uvm_object_utils_begin(uvme_example_env_cntxt_c)
+      `uvm_field_object(mapu_cntxt, UVM_DEFAULT)
+   `uvm_object_utils_end
+
+   /**
+    * Creates sub-context objects
+    */
+   function new(uvm_component parent=null, string name="uvme_example_env_cntxt");
+      super.new(parent, name);
+      cntxt = uvme_mapu_cntxt_c::type_id::create("mapu_cntxt");
+   endfunction
+
+   /**
+    * Creates sub-context objects
+    */
+   virtual function void create_objects();
+      mapu_cntxt = uvme_mapu_cntxt_c::type_id::create("mapu_cntxt");
+   endfunction
+
+endclass : uvme_example_env_cntxt_c
+
+
+`endif // __UVME_EXAMPLE_ENV_CNTXT_SV__
+
+
+`ifndef __UVME_EXAMPLE_ENV_SV__
+`define __UVME_EXAMPLE_ENV_SV__
+
+
+/**
+ * Component encapsulating the environment.
+ */
+class uvme_example_env_c extends uvmx_env_c #(
+   .T_CFG      (uvme_example_env_cfg_c      ),
+   .T_CNTXT    (uvme_example_env_cntxt_c    ),
+   .T_VSQR     (uvme_example_env_vsqr_c     ),
+   .T_PRD      (uvme_example_env_prd_c      ),
+   .T_SB       (uvme_example_env_sb_c       ),
+   .T_COV_MODEL(uvme_example_env_cov_model_c)
+);
+
+   uvme_mapu_env_c  mapu_env; ///< Matrix APU Block Environment instance.
+
+   `uvm_component_utils_begin(uvme_example_env_c)
+   `uvm_component_utils_end
+
+   /**
+    * Default constructor.
+    */
+   function new(uvm_component parent=null, string name="uvme_example_env");
+      super.new(parent, name);
+   endfunction
+
+   virtual function void assign_cfg();
+      uvm_config_db#(uvme_mapu_cfg_c)::set(this, "mapu_env", "cfg", cfg.mapu_cfg);
+   endfunction
+
+   virtual function void assign_cntxt();
+      uvm_config_db#(uvme_mapu_cntxt_c)::set(this, "mapu_env", "cntxt", cfg.mapu_cntxt);
+   endfunction
+
+   virtual function void create_envs();
+      mapu_env = uvme_mapu_env_c::type_id::create("mapu_env", this);
+   endfunction
+
+endclass : uvme_example_env_c
+
+
+`endif // __UVME_EXAMPLE_ENV_SV__
