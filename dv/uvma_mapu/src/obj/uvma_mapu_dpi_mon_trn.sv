12386ad19c62b423e2e2a2b56d8071c374f8c8e7
David Poulin <55469439+datum-dpoulin@users.noreply.github.com>
2023-03-22 20:02:37 -0400

    Update uvma_mapu_dpi_mon_trn.sv

--- dv/uvma_mapu/src/obj/uvma_mapu_dpi_mon_trn.sv
+++ dv/uvma_mapu/src/obj/uvma_mapu_dpi_mon_trn.sv
@@ -47,20 +47,20 @@ class uvma_mapu_dpi_mon_trn_c extends uvmx_mon_trn_c #(
     * Describes transaction for logger.
     */
    virtual function uvmx_metadata_t get_metadata();
+      string i_vld_str;
       string o_rdy_str;
       string i_r0_str;
       string i_r1_str;
       string i_r2_str;
-      if (i_vld === 1) begin
-         o_rdy_str = $sformatf("%h", o_rdy);
-         i_r0_str = $sformatf("%h", i_r0);
-         i_r1_str = $sformatf("%h", i_r1);
-         i_r2_str = $sformatf("%h", i_r2);
-         `uvmx_metadata_field("o_rdy", o_rdy_str)
-         `uvmx_metadata_field("i_r0", i_r0_str)
-         `uvmx_metadata_field("i_r1", i_r1_str)
-         `uvmx_metadata_field("i_r2", i_r2_str)
-      end
+      i_vld_str = $sformatf("%h", i_vld);
+      o_rdy_str = $sformatf("%h", o_rdy);
+      i_r0_str = $sformatf("%h", i_r0);
+      i_r1_str = $sformatf("%h", i_r1);
+      i_r2_str = $sformatf("%h", i_r2);
+      `uvmx_metadata_field("o_rdy", o_rdy_str)
+      `uvmx_metadata_field("i_r0", i_r0_str)
+      `uvmx_metadata_field("i_r1", i_r1_str)
+      `uvmx_metadata_field("i_r2", i_r2_str)
    endfunction
 
 endclass : uvma_mapu_dpi_mon_trn_c
